79529847ef7fa88e9596c14b58f9709988122854
AsgeirAlbretsen <bjellanda@gmail.com>
2018-06-25 21:48:00 +0200

    Initial Project Commit

---
+++ StomachRush/Assets/EnemyController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    Rigidbody2D rb;
+    Vector3 direction;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+
+        //PICK INITIAL DIRECTION
+        direction = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+	}
+
+    void Move()
+    {
+        rb.velocity = direction;
+    }
+
+    void ChangeDirection(Vector3 col)
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Vector3 dir = (transform.position - col.transform.position).normalized;
+        direction = dir + new Vector3(Random.Range(-1f,1f), Random.Range(-1f, 1f),0);
+    }
+}
---
+++ StomachRush/Assets/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D rb;
+    Camera cam;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        cam = GameObject.Find("MainCamera").GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButton("Fire1"))
+        {
+            Move(cam.ScreenToWorldPoint(Input.mousePosition));
+        }
+
+        if(Input.touchCount > 0)
+        {
+            Vector2 touchPosition = cam.ScreenToWorldPoint(Input.GetTouch(0).position);
+
+            MoveMobile(touchPosition);
+        }
+    }
+
+    void Move(Vector3 pos)
+    {
+        Vector3 rbPos = new Vector3(rb.position.x, rb.position.y, 0);
+        var heading = (pos - rbPos).normalized;
+        var distance = heading.magnitude*speed;
+        var rawHeading = pos - rbPos;
+        var rawDistance = rawHeading.magnitude;
+        if(rawDistance > 10.015f)
+        {
+            rb.velocity = heading * distance;
+        }
+    }
+
+    void MoveMobile(Vector3 pos)
+    {
+        Vector3 rbPos = new Vector3(rb.position.x, rb.position.y, 0);
+        var heading = (pos - rbPos).normalized;
+        var distance = heading.magnitude * speed;
+        var rawHeading = pos - rbPos;
+        var rawDistance = rawHeading.magnitude;
+        if (rawDistance > 10.015f)
+        {
+            rb.velocity = heading * distance;
+        }
+    }
+}
